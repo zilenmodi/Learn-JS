a28cb7e285f5bd81298d004cfdf81716c9c708b0
zilenmodi <zilen.m@simformsolutions.com>
2023-04-11 14:21:47 +0530

    Add webpack tut

---
+++ app/index.js
@@ -0,0 +1,11 @@
+//Why Webpack?
+/*
+This big file is called a "bundle" in Webpack parlance. A module bundler then is something which collating lots of small pieces into something larger and self-contained.
+
+The powerful ecosystem of Webpack plugins and extras makes Webpack seem confusing as it appears to do so much. All the extra things such as compiling ES6/7 to ES5, CSS modules and code splitting are just nice extras afforded to us by Webpack.
+*/
+
+const arr = ["surat", "rajkot", "ahmedabad"]
+console.log(arr);
+
+export default arr;
